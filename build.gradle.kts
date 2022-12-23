b05220946bbbcf8947e837f5fe6eb5e3997698b9
Wisoft Laboratory <wisoft@hanbat.ac.kr>
2022-12-23 21:38:49 +0900

    code

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootBuildImage
+
+plugins {
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.7.22"
+    kotlin("plugin.spring") version "1.7.22"
+}
+
+group = "io.wisoft"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<BootBuildImage> {
+    docker {
+        imageName.set(
+                "${
+                    System.getenv("DOCKER_ID")
+                }/${
+                    System.getenv("DOCKER_REPOSITORY")
+                }"
+        )
+        publish.set(true)
+        publishRegistry {
+            username.set(System.getenv("DOCKER_ID"))
+            password.set(System.getenv("DOCKER_PASSWORD"))
+        }
+    }
+}
---
+++ src/test/kotlin/io/wisoft/buildimagetutorial/BuildImageTutorialApplicationTests.kt
@@ -0,0 +1,26 @@
+package io.wisoft.buildimagetutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@WebMvcTest
+class BuildImageTutorialApplicationTests {
+
+    @Autowired
+    lateinit var client: MockMvc
+
+    @Test
+    fun `test hello`() {
+        client
+            .get("/")
+            .andExpect {
+                content {
+                    string("Hello Docker World")
+                }
+             }
+    }
+}
